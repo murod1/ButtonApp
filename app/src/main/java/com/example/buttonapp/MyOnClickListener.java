9272889ba767915541e59fca7e9f37cbff78e3b8
murod1 <jmurod2000@gmail.com>
2021-03-30 13:25:43 +0900

    listener wclass in a separate file

---
+++ app/src/main/java/com/example/buttonapp/MyOnClickListener.java
@@ -0,0 +1,13 @@
+package com.example.buttonapp;
+
+import android.view.View;
+
+public class MyOnClickListener implements View.OnClickListener {
+    MainActivity mainActivity;
+    public MyOnClickListener(MainActivity mainActivity){this.mainActivity = mainActivity;}
+
+    @Override
+    public void onClick(View v) {
+        mainActivity.mTextView1.setText("You clicked Murod's Button");
+    }
+}
